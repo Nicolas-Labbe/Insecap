025d5cac78a063209f80e02bc68c92ba397d042e
Nicolas Labbe <Nicolas Labbe@LAPTOP-SNFRE8VT>
2021-10-28 09:29:45 -0300

    Agregar archivos de proyecto.

---
+++ Insecap/Controllers/AlumnoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Insecap.Models;
+using Insecap.Models.ViewModels;
+
+namespace Insecap.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            List<ListAlumnoViewModel> lst;
+            using (InsecapEntities db = new InsecapEntities())
+            {
+                lst = (from d in db.alumno
+                           select new ListAlumnoViewModel
+                           {
+                               id_alumno = d.id_alumno,
+                               rut = d.rut,
+                               nombre = d.nombre,
+                               fecha_nacimiento = d.fecha_nacimiento,
+                               genero = d.genero
+                           }).ToList();
+            }
+            return View(lst);
+        }
+
+        public ActionResult Nuevo ()
+        {
+            return View();
+        }
+    }
+}
---
+++ Insecap/Models/ViewModels/ListAlumnoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Insecap.Models.ViewModels
+{
+    public class ListAlumnoViewModel
+    {
+        public int id_alumno { get; set; }
+        public string rut { get; set; }
+        public string nombre { get; set; }
+        public DateTime fecha_nacimiento { get; set; }
+        public string genero { get; set; }
+    }
+}
